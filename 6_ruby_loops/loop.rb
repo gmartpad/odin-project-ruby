e24be112d57a23b908dd019139ac3681e94db635
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-03-18 07:04:40 -0300

    [RUBY] ruby-loops - feat: read the ruby-loops chapter

---
+++ 6_ruby_loops/loop.rb
@@ -0,0 +1,6 @@
+i = 0
+loop do
+  puts "i is #{i}"
+  i += 1
+  break if i == 10
+end
---
+++ 6_ruby_loops/until_loop.rb
@@ -0,0 +1,11 @@
+i = 0
+until i >= 10 do
+  puts "i is #{i}"
+  i += 1
+end
+
+#
+
+until gets.chomp == "yes" do
+  puts "Will you go to prom with me?"
+end
