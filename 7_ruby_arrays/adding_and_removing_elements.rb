fee377f174e4368f6ab0cc9f0d6c15981b2184e5
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-03-18 23:20:21 -0300

    [RUBY] ruby-arrays - feat: read ruby-arrays article

---
+++ 7_ruby_arrays/adding_and_removing_elements.rb
@@ -0,0 +1,16 @@
+num_array = [1, 2]
+
+num_array.push(3, 4)      #=> [1, 2, 3, 4]
+num_array << 5            #=> [1, 2, 3, 4, 5]
+num_array.pop()           #=> 5
+num_array                 #=> [1, 2, 3, 4]
+
+num_array.unshift(1)      #=> [1, 1, 2, 3, 4]
+num_array.shift           #=> 1
+num_array                 #=> [2, 3, 4]
+
+num_array = [1, 2, 3, 4, 5, 6]
+
+num_array.pop(3)          #=> [4, 5, 6]
+num_array.shift(2)        #=> [1, 2]
+num_array                 #=> [3]
