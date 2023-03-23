867893c5614198c5e8e49cd70b137d0ffbd05631
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-03-22 21:59:24 -0300

    [RUBY] ruby-methods - feat: did the launch_school_exercises for the methods chapter

---
+++ 9_ruby_methods/launch_school_exercises/five.rb
@@ -0,0 +1,6 @@
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")       #=> it prints the words on the screen, but returns nil
---
+++ 9_ruby_methods/launch_school_exercises/four.rb
@@ -0,0 +1,7 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")       #=> it will not print anything
