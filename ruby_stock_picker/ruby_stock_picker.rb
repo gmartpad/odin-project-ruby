eaec210cb07379f31018249c4e0b16b197776031
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-04 00:10:21 -0300

    [RUBY] ruby-stock-picker - feat: did the ruby-stock-picker project

--- ruby_stock_picker/ruby_stock_picker.rb
+++ ruby_stock_picker/ruby_stock_picker.rb
@@ -1,30 +1,5 @@
 def stock_picker(array_of_prices)
-
-  lower_price = nil
-  higher_price = nil
-
-  lowest_prices = []
-  lowest_prices_indexes = []
-  highest_prices_indexes = []
-
-  array_of_prices.length.times do |number|
-    if ((lower_price != nil && lower_price > array_of_prices[number]) || lower_price == nil)
-      lower_price = array_of_prices[number]
-      lowest_prices.push(lower_price)
-      lowest_prices_indexes.push(array_of_prices.index(lower_price))
-    end
-  end
-
-  array_of_prices.length.times do |number|
-    if ((higher_price != nil && lowest_prices.min < array_of_prices[number]) || higher_price == nil)
-      higher_price = array_of_prices[number]
-      highest_prices_indexes.push(array_of_prices.index(higher_price))
-    end
-  end
-
-  p lowest_prices_indexes
-  p highest_prices_indexes
-
+  results = array_of_prices.each_with_index.to_a.combination(2).max_by { |buy, sell| sell[0]-buy[0] }.map { |price, index| index }
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
